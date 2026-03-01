16998789010154f2913c0da4b49c6d299912eb0f
Ish596 <gargish686@gmail.com>
2026-03-01 10:52:53 +0000

    Day 1 subtraction

---
+++ basic/subtraction.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class subtraction {
+    public static void main ( String[]args)
+{
+    Scanner sc = new Scanner ( System.in);
+    double a,b,sub;
+    System.out.print(" enter the value of a ");
+    a = sc.nextDouble();
+    System.out.print(" enter the value of b ");
+    b = sc.nextDouble();
+    sub = a-b;
+    System.out.print ( " the sub traction of numbers are " +sub);
+    sc.close();
+
+}}
