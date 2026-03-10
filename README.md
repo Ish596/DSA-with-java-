14668e03d326f7e5fe190303cfe02ff4390d8f70
Ish596 <gargish686@gmail.com>
2026-03-10 16:44:15 +0000

    switch statement

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # DSA-with-java-
 compleye dsa with java 
+-basic 
+1. addition program
+2. division program
+3. division program
+4. multiplication program 
+5. division program
+
---
+++ conditon/Switch.java
@@ -0,0 +1,50 @@
+import java.util.*;
+public class Switch
+{
+    public static void main( String []args)
+    {
+        Scanner sc = new Scanner (System.in);
+        double b,i, a;
+        System.out.print("enter 1st value  ");
+        a = sc.nextDouble();
+        System.out.print("enter 2nd value ");
+        b = sc.nextDouble();
+        System.out.println("enter the operator for operation = '+','-' '*' '/' '%' ");
+        char op = sc.next().charAt(0);
+    switch (op) 
+    {
+         case '+' :
+        i = a+b;
+        System.out.println(" the addition of digits are " +i);
+        break;
+
+        case '-':
+            i = a-b;
+            System.out.println(" the subtraction of digits are " +i);
+            break;
+
+            case'*':
+            i = a*b;
+            System.out.println(" the multiplication of digits are " +i);
+            break;
+
+        case '/':
+            i = a/b;
+            System.out.println(" the division of the digit are " +i);
+            break;
+
+            case '%':
+                i = a%b;
+                System.out.println("the modoulus of digit are " +i);
+                break;
+
+                default:
+                System.out.println("wrong choice ");
+
+
+    }
+    sc.close();
+
+
+    }
+}
